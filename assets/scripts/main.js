1aeb7d50d5daa00e99d03ef32cfa77cbc1049ea0
Wes Hardee <whardee@nerdery.com>
2014-04-25 22:30:00 -0500

    add active states for links & refactor class names

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,9 @@
 /*!
- * amanda-wedding v1.0.0 (dev)
+ * amanda-wedding v0.4 (dev)
  * Wedding website
  *
  * Copyright(c): 2014
- * Build Date: 2014-04-20
+ * Build Date: 2014-04-25
  */
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
